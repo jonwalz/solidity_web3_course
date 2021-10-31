ae36509c80a4a08b3f707653cfc674211d3a9d59
Jon <jon.walz.music@gmail.com>
2021-10-31 15:28:00 -0700

    update deploy code

--- deploy.js
+++ deploy.js
@@ -3,8 +3,22 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  'black solve wrestle slam spoon attitude tell pact easily rose display aware climb plate wall bind fly any island congress cloud nose holiday glad',
-  'https://rinkeby.infura.io/v3/380047a906df4799a23b2fb46fc3b0fe'
-)
+  "black solve wrestle slam spoon attitude tell pact easily rose display aware climb plate wall bind fly any island congress cloud nose holiday glad",
+  "https://rinkeby.infura.io/v3/380047a906df4799a23b2fb46fc3b0fe"
+);
 
-const web3 = new Web3(provider)
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account: ", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to: ", result.options.address);
+};
+
+deploy();
